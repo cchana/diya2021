4484961757c85b5c57d370d5e23804b02907db07
Charanjit Chana <cchana@gmail.com>
2021-06-18 09:11:17 +0100

    April 2021 updates with more questions and UI for answers

--- diya.js
+++ diya.js
@@ -1,10 +1,22 @@
 function sayHello() {
 
-  var name = prompt("What is your name?"),
-      colour = prompt("What is your favourite colour?");
+  var answers = {
+          name: prompt("What is your name?"),
+          colour: prompt("What is your favourite colour?").replace(' ', ''),
+          shape: prompt("What is your favorite shape?"),
+          hobby: prompt("what hobby keeps you busy?"),
+          animal: prompt("what animal do you love?"),
+          drink: prompt("what do you like drinking as a drink?")
+      };
 
-  document.getElementsByTagName('body')[0].style.backgroundColor = colour;
+  document.getElementsByTagName('body')[0].style.backgroundColor = answers.colour;
 
-  alert(`Hi, ${name}. Thank you for coming to my web page!`);
+  var text = ``;
+  for(question in answers) {
+    text += `<li>${question}: ${answers[question]}</li>`;
+  }
+
+  document.getElementById('you').innerHTML=text;
+//  alert(`Hi, ${name}. Thank you for coming to my web page!`);
 
 }
